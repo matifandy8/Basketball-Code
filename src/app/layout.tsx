b54806f1e30391d65fed8685e6a3b6bb44e243a3
unknown <matifandy@gmail.com>
2024-10-31 21:09:19 -0300

    feat: add sections nba a fiba home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+// className={`${geistSans.variable} ${geistMono.variable}`}
 
 export const metadata: Metadata = {
   title: "Basketball Code",
@@ -27,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Navbar />
         {children}
         <Footer />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,27 +14,65 @@ export default function Home() {
 
       <section className={styles.section}>
         <div className={styles.sectionContent}>
-          <h2 className={styles.sectionTitle}>NBA</h2>
-          <p>
-            The National Basketball Association (NBA) is a professional
-            basketball league in the United States. It is the most popular
-            professional basketball league in the world.
-          </p>
-          <Link href="/rules-nba" className={styles.link}>
-            Rules
-          </Link>
+          <div className={styles.sectionTextContainer}>
+            <h2 className={styles.sectionTitle}>NBA</h2>
+            <p className={styles.sectionText}>
+              The National Basketball Association (NBA) is a professional
+              basketball league in the United States. It is the most popular
+              professional basketball league in the world.
+            </p>
+            <Link href="/rules-nba" className={styles.link}>
+              Rules
+            </Link>
+          </div>
+          <div className={styles.sectionImageContainer}>
+            <Image
+              src="/images/nba.jpg"
+              alt="NBA logo"
+              className={styles.sectionImage}
+              priority
+              quality={100}
+              width={400}
+              height={400}
+              sizes="(max-width: 400px) 100vw, 400px"
+              style={{
+                objectFit: "cover",
+                objectPosition: "center",
+                borderRadius: "10%",
+              }}
+            />
+          </div>
         </div>
 
         <div className={styles.sectionContent}>
-          <h2 className={styles.sectionTitle}>FIBA</h2>
-          <p>
-            The Federative International Basketball Association (FIBA) is a
-            professional basketball league in the United States. It is the
-            second most popular professional basketball league in the world.
-          </p>
-          <Link href="/rules-fiba" className={styles.link}>
-            Rules
-          </Link>
+          <div className={styles.sectionImageContainer}>
+            <Image
+              src="/images/fiba.jpg"
+              alt="FIBA logo"
+              className={styles.sectionImage}
+              priority
+              quality={100}
+              width={400}
+              height={400}
+              sizes="(max-width: 400px) 100vw, 400px"
+              style={{
+                objectFit: "cover",
+                objectPosition: "center",
+                borderRadius: "10%",
+              }}
+            />
+          </div>
+          <div className={styles.sectionTextContainer}>
+            <h2 className={styles.sectionTitle}>FIBA</h2>
+            <p className={styles.sectionText}>
+              The French International Basketball Association (FIBA) is a
+              professional basketball league in France. It is the second most
+              popular professional basketball league in the world.
+            </p>
+            <Link href="/rules-fiba" className={styles.link}>
+              Rules
+            </Link>
+          </div>
         </div>
       </section>
     </main>
