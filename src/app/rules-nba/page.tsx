4864d4fc34452a01e86314c54c8d5c91f3a33318
unknown <matifandy@gmail.com>
2024-11-14 20:29:41 -0300

    feat: compress images and start with rules nba

--- src/app/rules-nba/page.tsx
+++ src/app/rules-nba/page.tsx
@@ -1,10 +1,38 @@
 import Hero from "../components/hero/hero";
+import styles from "./rules.module.css";
 
 export default function RulesNba() {
   return (
     <div>
       <Hero title="NBA Rules" imageUrl="/images/bg-nba.webp" />
-      <h1>NBA Rules</h1>
+      <div className={styles.rules}>
+        <div className={styles.rulesContent}>
+          <h2 className={styles.rulesTitle}>General Rules</h2>
+          <div className={styles.rulesList}>
+            <div className={styles.rulesListItem}>
+              <div className={styles.rulesListItemImage}>
+                <img
+                  alt="court nba"
+                  data-ll-status="loaded"
+                  src="/images/NBA-basketball-court-dimensions.jpg"
+                />
+              </div>
+              <div className={styles.rulesListItemText}>
+                <span>1.</span>
+                <div className={styles.rulesListItemTextContent}>
+                  <h3 className={styles.rulesListItemTextTitle}>
+                    COURT DIMENSIONS
+                  </h3>
+                  <p className={styles.rulesListItemTextDescription}>
+                    The NBA basketball court is 94 feet long and 50 feet wide.
+                    The height of the hoop is 7 feet.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
